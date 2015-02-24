2b3712ee0dc6131a80ba878ecb6a0760b1c6c8cc
Mike Hall <mph@puddingbowl.org>
2015-02-23 22:14:45 -0800

    Added user "handle" for no-namers, fixed sign-in/signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
+
+  def handle
+    if self.name
+      return self.name
+    else
+      return self.email
+    end
+
+  end
 end

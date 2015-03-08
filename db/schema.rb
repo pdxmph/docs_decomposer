179007746988a0c2c723dfcd56dc4d587d271d9d
Mike Hall <mph@puddingbowl.org>
2015-03-08 11:50:08 -0700

    Adding comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308183556) do
+ActiveRecord::Schema.define(version: 20150308184919) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "page_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "elements", force: :cascade do |t|
     t.string   "kind"

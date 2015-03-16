a37e529b244ac44f19271d5dc8823f044e8fbd94
Mike Hall <mph@puddingbowl.org>
2015-03-15 22:46:23 -0700

    Page with initial md5 stuff

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,20 +1,22 @@
 class Page < ActiveRecord::Base
-    acts_as_votable
-    acts_as_taggable
-    acts_as_taggable_on :priority,:risk,:keywords,:writer,:reviewers
-    has_many :comments
-    has_many :elements
-    accepts_nested_attributes_for :comments
+
+  validates_uniqueness_of :filename
+
+  acts_as_votable
+  acts_as_taggable
+  acts_as_taggable_on :priority,:risk,:keywords,:writer,:reviewers
+  has_many :comments
+  has_many :elements
+  accepts_nested_attributes_for :comments
 
     
-        def previous_page
-          self.class.where("id < ?", id).order("id desc").first
-        end
+  def previous_page
+    self.class.where("id < ?", id).order("id desc").first
+  end
 
-        def next_page
-          self.class.where("id > ?", id).order("id asc").first
-        end
-
+  def next_page
+    self.class.where("id > ?", id).order("id asc").first
+  end
     
 end
 

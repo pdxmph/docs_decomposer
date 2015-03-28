e9b560fbdbc18d2c06e4a5a7875a18598c9e1374
Mike Hall <mph@puddingbowl.org>
2015-03-28 01:15:13 -0700

    html_importer.rb now tries to fix image paths to serve them from public/puppet-docs

--- scripts/html_importer.rb
+++ scripts/html_importer.rb
@@ -5,11 +5,17 @@ require 'open-uri'
 pages = Page.all
 
 pages.each do |p|
+  images_path = p.filename.gsub(/(^.*\/)\w{1,}\.(md|markdown)/, "/puppet-docs/source\\1")
   begin
     puts p.url
 
     doc = Nokogiri::HTML(open(p.url))
     doc_content = doc.xpath("//div[@id='rendered-markdown']")
+    doc_content.xpath("//img").each do |i|
+      if i[:src].match(/^\.\/images\//)
+        i[:src] =  i[:src].gsub(/^\.\/images\//, "#{images_path}images/")
+      end
+    end
     p.content = doc_content.inner_html
     p.save
 

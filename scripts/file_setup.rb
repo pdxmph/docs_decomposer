e569c046ffcca221029cce574e28e14b71527ff7
Mike Hall <mph@puddingbowl.org>
2015-03-28 01:14:43 -0700

    file_setup.rb expects the puppet-docs submodule in public/puppet-docs

--- scripts/file_setup.rb
+++ scripts/file_setup.rb
@@ -1,28 +1,36 @@
 #!/usr/bin/env ruby
 
 
-projects = {"pe" => [3.3,3.7], "puppet" => [3.6, 3.7], "hiera" => [1]}
+projects = {"pe" => [3.7]}
 
 projects.each do |project,versions|
 
-  content_dir = "/Users/mike/Documents/puppet-docs/source/#{project}"
-
+  content_dir = File.expand_path("../../public/puppet-docs/source/#{project}", __FILE__)
+  
   versions.each do |version|
     dir = "#{content_dir}/#{version}"
     dir += "/reference" if project == "puppet"
    puts dir
 
-  Dir.foreach(dir) do |file|
+   Dir.foreach(dir) do |file|
     next unless file.match(/.markdown|.md/) 
+    file_html = file.gsub(/\.md|\.markdown/, ".html")
     src_yaml =  YAML.load_file("#{dir}/#{file}")
     puts src_yaml.inspect
     source_file = Page.new
     source_file.project = project
     source_file.version = version
-    source_file.filename = "/#{project}/#{version}/#{file}"
-    file_html = file.gsub(/\.md|\.markdown/, ".html")
-    source_file.url = "https://docs.puppetlabs.com/#{project}/#{version}/#{file_html}"
+
+    base_filename = "/#{project}/#{version}"
+    base_filename += "/reference" if project == "puppet"
+    source_file.filename = "#{base_filename}/#{file}"
+
+    base_url = "https://docs.puppetlabs.com/#{project}/#{version}"
+    base_url += "/reference" if project == "puppet"
+    source_file.url = "#{base_url}/#{file_html}"
+
     source_file.title = src_yaml['title'].strip
+
     source_file.save    
   end
   end

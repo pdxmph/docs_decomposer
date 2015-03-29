4b4527454d7ad8289dae46d5118dbb6ce9e223d1
Mike Hall <mph@puddingbowl.org>
2015-03-29 11:17:48 -0700

    Glossarizer removes puppety words from topics list

--- scripts/glossarizer.rb
+++ scripts/glossarizer.rb
@@ -1,10 +1,24 @@
-pages = Page.all
+pages = Project.find_by_name("pe").pages
+
+common_words = ["puppet", "pe", "enterprise", "n", "y", "-"]
 
 pages.each do |p|
-  next if p.version != "3.7"
- content = OTS.parse(Nokogiri::HTML(p.content).text)
+  next if p.version.version_number != "3.7"
+  html = Nokogiri::HTML(p.content)
+  elements = ["ol","pre","img", "ul"]
+  elements.each do |e|
+    html.xpath("//#{e}").remove
+end
 
- puts "#{p.title}: #{content.topics.to_sentence}"
+ text = html.text
 
-  
+ ots_content = OTS.parse(text)
+ 
+ topics  = ots_content.topics
+
+ common_words.each { |cw| topics.delete(cw) }
+
+ puts "#{p.title}: #{topics}"
+
+
 end

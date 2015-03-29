390893398b8dc4530cdfc2a604f6f7c8e61d3b9a
Mike Hall <mph@puddingbowl.org>
2015-03-28 19:48:11 -0700

    Comment deletions/changes no longer require a reload

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,24 +3,25 @@ class CommentsController < ApplicationController
 
 def update
   @comment = Comment.find(params[:id])
- 
+  @page = @comment.page
   if @comment.update(:content => params[:comment][:content])
     respond_to do |format|
-      format.html {redirect_to :back}
+      format.js {render :action => 'update.js.haml', :object => @page}
     end
     else
       respond_to do |format|
         format.html {redirect_to :back, :alert => "Didn't save your change."}
       end
-     end
+   end
 end
 
   
   def create
     @comment = Comment.new(:content => params[:comment][:content], :user_id => params[:comment][:user_id], :page_id => params[:comment][:page_id])
     if @comment.save
+      @page = @comment.page
       respond_to do |format|
-        format.html {redirect_to :back, :notice => "Comment saved."}
+        format.js {render :action => 'update.js.haml',:object => @page}
       end
     else
       respond_to do |format|
@@ -33,7 +34,7 @@ end
     @comment = Comment.find params[:id]
     if @comment.destroy
       respond_to do |format|
-       format.js
+       format.js {render :action => 'update.js.haml', :object => @page}
        format.html {redirect_to :back, :notice => "Comment deleted."}
       end
     end

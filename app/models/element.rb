b4c3f4eeb2ba0c1977b5f7495233fe0bf5a1d4f1
Mike Hall <mph@puddingbowl.org>
2015-04-18 04:28:45 -0700

    Add elements tab + element validation

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,6 +1,24 @@
 class Element < ActiveRecord::Base
-
+  validates :page_id, uniqueness: {scope: :checksum}
   belongs_to :page
 
+
+  def kind_name
+    case kind
+    when "ol"
+      "Ordered List"
+    when "ul"
+      "Unordered List"
+    when "pre"
+      "Pre block"
+    when "img"
+      "Image"
+    end
+  end
+
+  def others
+   Element.where("checksum = ? AND page_id != ?",checksum, page_id)
+  end
+      
   
 end

5836838eb4c2c7aaf8c98cd94d3f626eace6d101
Michael Hall <michael.hall@puppetlabs.com>
2015-05-27 12:38:05 -0700

    Turns on recoverable add-in for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :recoverable
 
   scope :admins, -> {where(:admin => true)}
-
+    
   def commented_pages
     pages = self.comments.pluck(:page_id)
     pages.uniq!
     return Page.where(id: pages)
   end
-
   
   def handle
     self.fullname ? self.fullname : self.email
   end
 
   def has_pages?
-	  true ? self.pages.count > 0 : false
+    true ? self.pages.count > 0 : false
   end
 
   def has_jira_name
     true ? self.jira_name != nil : false
   end
-  
+ 
 end

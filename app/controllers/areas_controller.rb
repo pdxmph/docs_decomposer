eb696611d2001f845e229108f369e7741cbe5f26
Mike Hall <mph@puddingbowl.org>
2015-05-27 22:36:06 -0700

    WIP on adding areas

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,21 @@
 class AreasController < ApplicationController
+
+  def index
+    @areas = Area.all
+    @title = "Documentation Capacity"
+    render :template => "areas/index", :locals => {:areas => @areas}   
+  end
+
+  def new
+    @area = Area.new
+  end
+
+  def create
+    @area = Area.new(:name => params[:area][:name], :priority => params[:area][:priority], :frequency => params[:area][:frequency], :state => params[:area][:state])
+    if @area.save
+      respond_to do |format|
+        format.html {redirect_to areas_path, :alert => "Area saved."}
+      end
+    end
+  end
 end

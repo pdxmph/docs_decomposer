cb31460e365108e27293d26bbea767e8116ad252
Mike Hall <mph@puddingbowl.org>
2015-05-30 18:53:36 -0700

    Improve visualization of capacity

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,9 +2,7 @@ class Area < ActiveRecord::Base
 
   def support_status
 
-    if self.priority == nil || self.writer_coverage == nil
-      0
-    elsif self.priority == 0 || self.writer_coverage == 0
+    if self.priority == nil || self.writer_coverage == nil || self.priority == 0 
       0
     elsif self.priority > self.writer_coverage + 1
       3
